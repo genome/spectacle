1651e9d57dd651a6969d9f95062fffcf50345512
Adam Coffman <acoffman@genome.wustl.edu>
2014-05-07 13:03:01 -0500

    merge conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def blah
+    ap = AnalysisProject.find(params[:analysis_project_id])
+    render json: AlignmentMetricsQuery.new(ap.models.pluck(:id), 'bwa').execute.to_json
+  end
 end
