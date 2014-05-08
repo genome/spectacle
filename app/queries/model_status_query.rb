53dc6ed2e2e2a6b6810f945930b2f6520984c13e
Thomas Mooney <tmooney@genome.wustl.edu>
2014-05-08 17:46:15 -0500

    Status is now available directly on build.

--- app/queries/model_status_query.rb
+++ app/queries/model_status_query.rb
@@ -9,7 +9,7 @@ class ModelStatusQuery
 
   private
   def build_query(model_ids)
-    first_in_clause = ActiveRecord::Base.send(:sanitize_sql_array, ['event.model_id IN (?)', model_ids])
+    first_in_clause = ActiveRecord::Base.send(:sanitize_sql_array, ['build.model_id IN (?)', model_ids])
     second_in_clause = ActiveRecord::Base.send(:sanitize_sql_array, ['model.genome_model_id IN (?)', model_ids])
     query(first_in_clause, second_in_clause)
   end
@@ -18,21 +18,19 @@ class ModelStatusQuery
     %{
       SELECT
         CASE WHEN model.build_requested THEN 'Build Requested'
-          WHEN event.event_status IS NULL THEN 'Buildless'
-          ELSE event.event_status
+          WHEN build.status IS NULL THEN 'Buildless'
+          ELSE build.status
         END AS status,
         model.genome_model_id AS model_id
       FROM model.model model
       LEFT JOIN (SELECT model_id, MAX(date_scheduled) AS date_scheduled
-                  FROM model.event
-                  WHERE event_status != 'Abandoned'
+                  FROM model.build
+                  WHERE status != 'Abandoned'
                     AND #{first_in_clause}
-                    AND event_type = 'genome model build'
                   GROUP BY model_id) date_scheduled
         ON model.genome_model_id = date_scheduled.model_id
-      LEFT JOIN model.event event ON model.genome_model_id = event.model_id
-        AND date_scheduled.date_scheduled = event.date_scheduled
-        AND event.event_type = 'genome model build'
+      LEFT JOIN model.build build ON model.genome_model_id = build.model_id
+        AND date_scheduled.date_scheduled = build.date_scheduled
       WHERE #{second_in_clause};
     }
   end
