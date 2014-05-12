27b2e46e3056c080bf2f8cf4ab917b43b37399b7
Jim Weible <jweible@genome.wustl.edu>
2014-05-12 13:11:12 -0500

    add software_result accessor

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,5 +2,7 @@ class Build < ActiveRecord::Base
   self.table_name = 'model.build'
 
   has_many :events
+  has_many :software_result_users, foreign_key: :user_id
+  has_many :software_results, through: :software_result_users
   belongs_to :model
 end
