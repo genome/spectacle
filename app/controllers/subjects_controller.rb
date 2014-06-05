41cfe4eef2e5a1e40821d6411f9bf03aa72a50c5
Thomas Mooney <tmooney@genome.wustl.edu>
2014-06-05 13:37:01 -0500

    Add libraries to subject overview

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -3,5 +3,6 @@ class SubjectsController < ApplicationController
     @subject = Subject.where(:subject_id => params[:id]).first!
     @attributes = @subject.subject_attributes
     @models = @subject.models.page(params[:page])
+    @libraries = @subject.libraries
   end
 end
